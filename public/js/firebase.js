2feba8e067bdddcb70fceff10c002442be8924e8
David Warutumo <davidwarchy@gmail.com>
2022-08-26 01:39:16 +0300

    added firebase db facilities

---
+++ public/js/firebase.js
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAhuEY0PJhIB5eNaxdlsBSyVgDeGa3wZcc",
+    authDomain: "blog-b4305.firebaseapp.com",
+    projectId: "blog-b4305",
+    storageBucket: "blog-b4305.appspot.com",
+    messagingSenderId: "818719375877",
+    appId: "1:818719375877:web:7c155556408d179f65926c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  let db = firebase.firestore();
+
+  
